7739f404a192cd3bba8a5e9aba1d202e5957213d
mstoll <mstoll@MstollHome.localdomain>
2018-02-18 18:37:30 +0100

    initial

---
+++ createOriginCluster.sh
@@ -0,0 +1,17 @@
+# https://github.com/Microsoft/openshift-origin
+
+rg="mdp-origin"
+
+echo "Create the resource group"
+az group create -n "$rg" -l WestEurope
+
+
+echo "Create the K8s by deploying the generated ARM template"
+az group deployment validate \
+    --resource-group "$rg" \
+    --template-file "azuredeploy.json" \
+    --parameters "./azuredeploy.parameters.json"
+
+
+
+ssh-agent bash -c 'ssh-add ~/.ssh/id_rsa; git push orgin master'
