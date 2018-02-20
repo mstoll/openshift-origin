30f4c48804de1379a73dff50cbe8701d61abdfe3
matthias Stollenwerk <matthiasstollenwerk@matthiasMacBook.fritz.box>
2018-02-20 13:18:41 +0100

    working

--- createOriginCluster.sh
+++ createOriginCluster.sh
@@ -9,36 +9,36 @@ nodeSubnetName="nodeSubnet"
 vnetCidr="10.0.0.0/16"
 masterSubenetCidr="10.0.0.0/24"
 nodeSubnetCidr="10.0.1.0/24"
+keyVaultname="mdp-keyvault"
+secretName="sshPrivateKey"
 
+##
+##
+##
+# az group create -n "$networkRg" -l WestEurope
 
-az group create -n "$networkRg" -l WestEurope
+# echo "Create a VNET to deploy K8s into an existing VNET"
+#  az network vnet create \
+# --name $vnetName \
+# --resource-group $networkRg \
+# --location westeurope \
+# --address-prefix $vnetCidr \
+# --subnet-name $subnetName \
+# --subnet-prefix $subnetCidr
 
-echo "Create a VNET to deploy K8s into an existing VNET"
- az network vnet create \
---name $vnetName \
---resource-group $networkRg \
---location westeurope \
---address-prefix $vnetCidr \
---subnet-name $subnetName \
---subnet-prefix $subnetCidr
-
-
-
+##
+##
+##
 echo "Create the resource group"
-az group create -n "$rg" -l WestEurope
+az group create -n "$rg" -l $location
 
+az keyvault create -n $keyVaultname -g $rg -l $location --enabled-for-template-deployment true
+az keyvault secret set --vault-name $keyVaultname -n $secretName --file ~/.ssh/id_rsa
+
+# az group deployment validate --resource-group "$rg" --template-file "azuredeploy.json" --parameters "./azuredeploy.parameters.json"
 
 echo "Create the K8s by deploying the generated ARM template"
-az group deployment create \
-    --name "Fuckit" \
-    --resource-group "$rg" \
-    --template-file "azuredeploy.json" \
-    --parameters "./azuredeploy.parameters.json"
+az group deployment create --debug --name "mdp-deployment1" --resource-group "$rg" --template-file "azuredeploy.json" --parameters "./azuredeploy.parameters.json" | tee openshift.log
 
-
-
-ssh-agent bash -c 'ssh-add ~/.ssh/id_rsa; git push orgin master'
-
-
-
+# az group delete -y --name $rg
 
